06735c2ca696fd5152a7dde4f8231a4eeb602457
Christopher Piggott <cpiggott@gmail.com>
2026-09-07 11:22:10 -0400

    Refactor from CSS to SASS

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.scss";
+import "./styles/index.scss";
 
 import App from "./app/App";
 import { AuthProvider } from "./auth/AuthProvider";
